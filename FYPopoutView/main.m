c7a1cc373c75997fa911c67b2bdc3421e06c53a7
fanyang <1050549505@qq.com>
2017-06-13 17:34:58 +0800

    Initial Commit

---
+++ FYPopoutView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FYPopoutView
+//
+//  Created by 樊杨 on 2017/6/13.
+//  Copyright © 2017年 YHCRT.FY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
